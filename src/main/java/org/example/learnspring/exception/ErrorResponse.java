b1af0513316b9940d54d9ebec18fa57655c5739d
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2026-06-11 22:05:03 +0530

    feat: Add global exception handling with custom error responses

---
+++ src/main/java/org/example/learnspring/exception/ErrorResponse.java
@@ -0,0 +1,17 @@
+package org.example.learnspring.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    private Map<String,String> errors;
+
+}
---
+++ src/main/java/org/example/learnspring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package org.example.learnspring.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex){
+        Map<String,String> fieldErrors= new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName=((FieldError)error).getField();
+            String errorMessage=error.getDefaultMessage();
+            fieldErrors.put(fieldName,errorMessage);
+        });
+
+        ErrorResponse response=new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "validation error",
+                LocalDateTime.now(),
+                fieldErrors
+        );
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+        }
+
+        @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+        ErrorResponse response=new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(),
+                LocalDateTime.now(),
+                null);
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+
+        }
+        @ExceptionHandler(Exception.class)
+    public  ResponseEntity<ErrorResponse> handleGeneralException(Exception ex){
+        ErrorResponse response=new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected server error occured, please try again later",
+                LocalDateTime.now(),
+                null
+        );
+        return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+}
